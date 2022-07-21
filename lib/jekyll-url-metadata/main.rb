d0d15bda05a830708b063e110249a93f3957df31
Gourav Khunger <gouravkhunger18@gmail.com>
2022-07-21 23:32:41 +0530

    feat: caching logic for retreived data + custom timeout configs
    release v1.0.1 🎉

--- lib/jekyll-url-metadata/main.rb
+++ lib/jekyll-url-metadata/main.rb
@@ -4,6 +4,15 @@ require "nokogiri"
 module Jekyll
   module URLMetadata
 
+    def url_config
+      @@url_config ||= @context.registers[:site].config['url_metadata'].nil? ? {} : 
+        @context.registers[:site].config['url_metadata']
+    end
+
+    def cache
+      @@cache ||= Jekyll::Cache.new("Jekyll::URLMetadata")
+    end
+
     def metadata(input)
       if !input.is_a?(String)
         log("Expected input type \"String\". Got \"#{input.class}\".")
@@ -15,10 +24,34 @@ module Jekyll
         return
       end
 
+      if !url_config["open_timeout"].nil? && !url_config["open_timeout"].is_a?(Integer)
+        log("Expected an \"Integer\" value for config \"open_timeout\". Got #{url_config["open_timeout"]}.")
+
+        return
+      end
+
+      if !url_config["read_timeout"].nil? && !url_config["read_timeout"].is_a?(Integer)
+        log("Expected an \"Integer\" value for config \"read_timeout\". Got #{url_config["read_timeout"]}.")
+
+        return
+      end
+
+      # The getset() API implements a cache first strategy
+      cache.getset(input) do
+        generate_hashmap(input)
+      end
+    end
+
+    def generate_hashmap(input)
       # parse HTML from URL
-      doc = Nokogiri::HTML(URI.open(input))
+      doc = Nokogiri::HTML(URI.open(input, { 
+        :open_timeout => url_config["open_timeout"].nil? ? 1 : url_config["open_timeout"],
+        :read_timeout => url_config["read_timeout"].nil? ? 1 : url_config["read_timeout"]
+      }))
+
       if !doc
         log("Failed to parse HTML from #{input}. Please double check for URL validity.")
+        return
       end
 
       hash = Hash.new
