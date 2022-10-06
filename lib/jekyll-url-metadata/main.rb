a75f06ee80539a765020b6dc2c6bf737c07765d7
Gourav Khunger <gouravkhunger18@gmail.com>
2022-10-06 23:58:56 +0530

    feat: add tests

--- lib/jekyll-url-metadata/main.rb
+++ lib/jekyll-url-metadata/main.rb
@@ -5,8 +5,12 @@ module Jekyll
   module URLMetadata
 
     def url_config
-      @@url_config ||= @context.registers[:site].config['url_metadata'].nil? ? {} : 
-        @context.registers[:site].config['url_metadata']
+      begin
+        @@url_config ||= @context.registers[:site].config['url_metadata'].nil? ? {} : 
+          @context.registers[:site].config['url_metadata']
+      rescue
+        {}
+      end
     end
 
     def cache
@@ -14,27 +18,7 @@ module Jekyll
     end
 
     def metadata(input)
-      if !input.is_a?(String)
-        log("Expected input type \"String\". Got \"#{input.class}\".")
-        return
-      end
-
-      if input.nil? || input == ""
-        log("Empty input string.")
-        return
-      end
-
-      if !url_config["open_timeout"].nil? && !url_config["open_timeout"].is_a?(Integer)
-        log("Expected an \"Integer\" value for config \"open_timeout\". Got #{url_config["open_timeout"]}.")
-
-        return
-      end
-
-      if !url_config["read_timeout"].nil? && !url_config["read_timeout"].is_a?(Integer)
-        log("Expected an \"Integer\" value for config \"read_timeout\". Got #{url_config["read_timeout"]}.")
-
-        return
-      end
+      return if !is_input_valid(input) || !is_config_valid()
 
       # The getset() API implements a cache first strategy
       cache.getset(input) do
@@ -42,15 +26,15 @@ module Jekyll
       end
     end
 
-    def generate_hashmap(input)
+    def generate_hashmap(input, testCase = false)
       # parse HTML from URL
-      doc = Nokogiri::HTML(URI.open(input, { 
-        :open_timeout => url_config["open_timeout"].nil? ? 1 : url_config["open_timeout"],
-        :read_timeout => url_config["read_timeout"].nil? ? 1 : url_config["read_timeout"]
-      }))
-
-      if !doc
-        log("Failed to parse HTML from #{input}. Please double check for URL validity.")
+      begin
+        doc = Nokogiri::HTML(URI.open(input, { 
+          :open_timeout => url_config["open_timeout"].nil? ? 1 : url_config["open_timeout"],
+          :read_timeout => url_config["read_timeout"].nil? ? 1 : url_config["read_timeout"]
+        }))
+      rescue
+        log("Failed to parse HTML from #{input}. Please double check for URL validity.") if !testCase
         return
       end
 
@@ -86,8 +70,46 @@ module Jekyll
       hash
     end
 
-    def log(msg)
-      Jekyll.logger.error "URL Metadata:", msg
+    def is_input_valid(input, testCase = false)
+      if !input.is_a?(String)
+        log("Expected input type \"String\". Got \"#{input.class}\".") if !testCase
+
+        return false
+      end
+
+      if input.nil? || input == ""
+        log("Empty input string.") if !testCase
+
+        return false
+      end
+
+      begin
+        if URI.parse(input).kind_of?(URI::HTTP)
+          return true
+        else
+          log("'#{input}' does not seem to be a valid URL.") if !testCase
+        end
+      rescue
+        log("'#{input}' does not seem to be a valid URL.") if !testCase
+      end
+
+      false
+    end
+
+    def is_config_valid()
+      if !url_config["open_timeout"].nil? && !url_config["open_timeout"].is_a?(Integer)
+        log("Expected an 'Integer' value for config 'open_timeout'. Got #{url_config["open_timeout"].class}.")
+
+        return false
+      end
+
+      if !url_config["read_timeout"].nil? && !url_config["read_timeout"].is_a?(Integer)
+        log("Expected an 'Integer' value for config 'read_timeout'. Got #{url_config["read_timeout"].class}.")
+
+        return false
+      end
+
+      true
     end
 
     def exists(obj)
@@ -96,6 +118,10 @@ module Jekyll
 
     def get(obj, attr)
       obj.get_attribute(attr)
+    end
+
+    def log(msg)
+      Jekyll.logger.error "URL Metadata:", msg
     end
 
   end # module Jekyll
